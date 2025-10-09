0a2e9b15e847ba34554ff8f199b269c383da6a51
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-10-09 17:16:41 +0100

    objects: introduce interpolation validity function

--- src/libtrx/game/items/anim.c
+++ src/libtrx/game/items/anim.c
@@ -146,21 +146,11 @@ int32_t Item_GetFrames(const ITEM *item, ANIM_FRAME *frames[], int32_t *rate)
         return numerator;
     }
 
-    // Avoid the flip 180 command having a bad effect on interpolated frames
-    // on rate 1 animations, such as neutral jump twist. TODO: improve this.
-    if (key_frame_span == 1
-        && Anim_HasFXCommandBetween(
-            anim, ITEM_ACTION_TURN_180, first_key_frame_num,
-            second_key_frame_num)) {
-        *rate = denominator;
-        return numerator;
-    }
-
     // Invalid state for interpolation
-    if (item != Lara_GetItem()
-        && (!item->active || item->status != IS_ACTIVE
-            || !item->enable_interpolation
-            || !Object_Get(item->object_id)->enable_interpolation)) {
+    const OBJECT *const obj = Object_Get(item->object_id);
+    if (obj->can_interpolate_func != nullptr
+        && !obj->can_interpolate_func(
+            item, first_key_frame_num, second_key_frame_num)) {
         *rate = denominator;
         return numerator;
     }
